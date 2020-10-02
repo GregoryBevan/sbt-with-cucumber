0804793903e801ff836b1e0a337e4604699b30f7
Gregory Bevan <gregory.bevan@zenika.com>
2020-10-02 15:04:02 +0200

    ✅ Add a simple Junit5 test

---
+++ src/test/java/Junit5SampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Junit5SampleTest {
+
+    @Test
+    void simpleTest() {
+        assertEquals(8, 5+3);
+    }
+
+}
